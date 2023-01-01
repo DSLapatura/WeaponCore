81ac0c67f0212b9794089cd6f8c0efb9f45d853b
Shane Stixrud <shane@stixrud.org>
2022-12-31 19:10:41 -0800

    sync

--- Data/Scripts/CoreSystems/Ai/AiSupport.cs
+++ Data/Scripts/CoreSystems/Ai/AiSupport.cs
@@ -436,11 +436,11 @@ namespace CoreSystems.Support
                 if (aiFaction != null)
                 {
                     BgFactionColor = MyColorPickerConstants.HSVOffsetToHSV(aiFaction.CustomColor).HSVtoColor().ToVector4().ToLinearRGB();
-                    BgFactionColor *= 10;
-                    BgFactionColor.W *= 0.1f;
+                    BgFactionColor *= 100;
+                    BgFactionColor.W *= 0.01f;
                     FgFactionColor = MyColorPickerConstants.HSVOffsetToHSV(aiFaction.IconColor).HSVtoColor().ToVector4().ToLinearRGB();
-                    FgFactionColor *= 10;
-                    FgFactionColor.W *= 0.1f;
+                    FgFactionColor *= 100;
+                    FgFactionColor.W *= 0.01f;
                 }
                 else
                 {
